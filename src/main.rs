4c8c6e098f6599753d77edc1e140da7da82c9486
Andrew Johnson <andrew@subarctic.org>
2023-07-04 16:11:55 -0600

    load policy from file then apply policy to stdin

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Policy {
+}
+
 fn main() {
+   let mut policy = Policy {};
    let mut hard = true;
    for arg in std::env::args().skip(1) {
       if arg=="hard" { hard = true; }
       else if arg=="soft" { hard = false; }
-      else if hard { eval_hard(&arg); }
-      else         { eval_soft(&arg); }
+      else         { load_policy(&mut policy, &arg); }
    }
+   if hard { eval_hard(&policy); }
+   else { eval_soft(&policy); }
 }
 
-fn eval_soft(filename: &str) {
-   let mut policy = String::new();
-   let mut file = File::open(filename).expect("eval_soft: error opening file");
-   file.read_to_string(&mut policy).expect("eval_soft: unable to read to string");
-   println!("eval soft:\n{policy}\n");
+fn load_policy(policy: &mut Policy, filename: &str) {
+   let mut p = String::new();
+   let mut file = File::open(filename).expect("load_policy: error opening file");
+   file.read_to_string(&mut p).expect("load_policy: unable to read to string");
+   println!("load_policy: {p}");
 }
 
-fn eval_hard(filename: &str) {
-   let mut policy = String::new();
-   let mut file = File::open(filename).expect("eval_hard: error opening file");
-   file.read_to_string(&mut policy).expect("eval_hard: unable to read to string");
-   println!("eval hard:\n{policy}\n");
+fn eval_soft(policy: &Policy) {
+   println!("eval soft\n");
+}
+
+fn eval_hard(policy: &Policy) {
+   println!("eval hard\n");
 }
