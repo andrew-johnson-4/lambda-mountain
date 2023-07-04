b730c6a95261e8be53bb22f31d17a2de2bfcda77
Andrew Johnson <andrew@subarctic.org>
2023-07-04 15:07:17 -0600

    define entry points for hard/soft eval

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 
 fn main() {
+   let mut hard = true;
+   for arg in std::env::args().skip(1) {
+      if arg=="hard" { hard = true; }
+      else if arg=="soft" { hard = false; }
+      else if hard { eval_hard(&arg); }
+      else         { eval_soft(&arg); }
+   }
+}
+
+fn eval_soft(filename: &str) {
+   println!("eval soft {filename}");
+}
+
+fn eval_hard(filename: &str) {
+   println!("eval hard {filename}");
 }
