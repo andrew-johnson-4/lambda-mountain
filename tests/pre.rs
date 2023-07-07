10c6fe9a7b322dd4549758d2cd687c4703b1281b
Andrew Johnson <andrew@subarctic.org>
2023-07-06 21:13:34 -0600

    introduce the concept of patterns in parser

--- tests/pre.rs
+++ tests/pre.rs
@@ -23,3 +23,30 @@ fn print_echo2() {
 
    assert_eq!( p.soft("a"), "aa" );
 }
+
+#[test]
+fn print_pat1() {
+   let mut p = Policy::new();
+   p.load(r#"::pre := λ["] s. s\n::pre := λs. s"#);
+
+   assert_eq!( p.soft("\"a"), "a" );
+   assert_eq!( p.soft("a"), "a" );
+}
+
+#[test]
+fn print_pat2() {
+   let mut p = Policy::new();
+   p.load(r#"::pre := λs ["]. s\n::pre := λs. s"#);
+
+   assert_eq!( p.soft("a\""), "a" );
+   assert_eq!( p.soft("a"), "a" );
+}
+
+#[test]
+fn print_pat3() {
+   let mut p = Policy::new();
+   p.load(r#"::pre := λ["] s ["]. s\n::pre := λs. s"#);
+
+   assert_eq!( p.soft("\"a\""), "a" );
+   assert_eq!( p.soft("a"), "a" );
+}
