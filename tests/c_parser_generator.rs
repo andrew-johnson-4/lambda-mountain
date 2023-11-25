9cdd609f04bffd0cb724581987d91392bf46f8ef
Andrew Johnson <andrew@subarctic.org>
2023-11-24 18:05:50 -0700

    define style for context-free grammars

--- tests/c_parser_generator.rs
+++ tests/c_parser_generator.rs
@@ -31,3 +31,37 @@ fn cfg1() {
       "Parse Error: Expected EOF at line 1, column 2"
    );
 }
+
+#[test]
+fn cfg2() {
+   let mut grammar = compile(r#"expr := λ(expr-mul x) /+/ (expr y). + x y
+expr := λ(expr-mul x) /-/ (expr y). - x y
+expr-mul := λ(expr-atom x) /*/ (expr-mul y). * x y
+expr-mul := λ(expr-atom x) /// (expr-mul y). / x y
+expr-atom := λ(: x /[0-9]+/). x"#);
+   assert_eq!(
+      grammar.run("expr","1").to_string(),
+      "Parse Result: 1"
+   );
+   assert_eq!(
+      grammar.run("expr","1+2").to_string(),
+      "Parse Result: (+ 1 2)"
+   );
+   assert_eq!(
+      grammar.run("expr","1-2").to_string(),
+      "Parse Result: (- 1 2)"
+   );
+   assert_eq!(
+      grammar.run("expr","1*2").to_string(),
+      "Parse Result: (* 1 2)"
+   );
+   assert_eq!(
+      grammar.run("expr","1+2").to_string(),
+      "Parse Result: (/ 1 2)"
+   );
+
+   assert_eq!(
+      grammar.run("expr","1*2+3/4-5").to_string(),
+      "Parse Result: - (+ (* 1 2) (/ 3 4)) 5"
+   );
+}
