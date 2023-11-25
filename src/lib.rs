2443b45784aa788fa313607cbe1c01a7bb7bf27c
Andrew Johnson <andrew@subarctic.org>
2023-11-25 01:50:16 -0700

    license notes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+/*
+
+Copyright 2023 - Andrew Johnson
+
+This code and all related intellectual property is available under the terms of
+the attached permissive MIT license. This license is intended only to protect
+the future development of the project while otherwise allowing people to use
+the code and IP as they would like. Please, just be nice.
+
+*/
 
 pub mod ast; pub use crate::ast::*;
 
