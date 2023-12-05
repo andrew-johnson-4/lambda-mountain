b4771472d4ac4c7f962bf9980687e4f8e67bda9c
Andrew Johnson <andrew@subarctic.org>
2023-12-05 15:14:54 -0700

    parser seems to be working

--- src/p.rs
+++ src/p.rs
@@ -13,27 +13,18 @@ P: A fast String to AST parser
 
 use crate::*;
 
-pub fn parse_expression(s: &str) -> S {
-   s_nil()
-}
-
-pub fn parse_program(s: &str) -> S {
-   s_nil()
-}
-
-pub fn parse_many_rhs(input: &str) -> S {
-   s_nil()
-}
-
-pub fn parse_one_rhs(input: &str) -> S {
+fn parse_one_expression(input: &str) -> S {
    let input = input.trim();
    if input.starts_with("λ") {
       if let Some((lhs,rhs)) = input.strip_prefix("λ").unwrap().split_once(".") {
-         lambda( parse_many_rhs(lhs), parse_many_rhs(rhs) )
+         lambda( parse_expression(lhs), parse_expression(rhs) )
       } else { s_nil() }
+   } else if input.len()>=2 && input.starts_with("/") && input.ends_with("/") {
+      let input = input.strip_prefix("/").unwrap().strip_suffix("/").unwrap();      
+      regex(input)
    } else if input.starts_with("(") && input.ends_with(")") {
       let input = input.strip_prefix("(").unwrap().strip_suffix(")").unwrap();
-      parse_many_rhs(input)
+      parse_expression(input)
    } else {
       let cs = input.to_string();
       let c = cs.chars().next().unwrap();
@@ -43,4 +34,41 @@ pub fn parse_one_rhs(input: &str) -> S {
          literal(&cs)
       }
    }
+}
+
+//All strings are valid expressions, this function is total
+pub fn parse_expression(input: &str) -> S {
+   let input = input.trim();
+   let mut depth = 0;
+   let mut buf = String::new();
+   let mut terms = Vec::new();
+   for c in input.chars() {
+      if c=='(' { depth += 1; buf.push(c); }
+      else if c==')' { depth -= 1; buf.push(c); }
+      else if c=='λ' { depth += 1; buf.push(c); }
+      else if c==' ' {
+         if depth>0 { buf.push(c); }
+         else if buf.len()>0 {
+            terms.push(parse_one_expression(&buf));
+            buf = String::new();
+         }
+      } else { buf.push(c); }
+   }
+   if buf.len()>0 { terms.push(parse_one_expression(&buf)) }
+   if terms.len()==0 { return s_nil(); }
+   let mut f = terms[0].clone();
+   for x in &terms[1..] {
+      f = app( f, x.clone() );
+   }
+   f
+}
+
+//All strings are valid programs, this function is total
+pub fn parse_program(s: &str) -> S {
+   let mut kvs = Vec::new();
+   for line in s.split("\n") {
+   if let Some((l,r)) = line.split_once(":=") {
+      kvs.push(( s_atom(l), parse_expression(r) ));
+   }}
+   kv(&kvs)
 }
--- tests/b_p.rs
+++ tests/b_p.rs
@@ -4,12 +4,14 @@ use lambda_mountain::*;
 #[test]
 fn expressions() {
    assert_eq!( parse_expression("a").to_string(), "(variable . a)" );
-   assert_eq!( parse_expression("123").to_string(), "(literal . a)" );
+   assert_eq!( parse_expression("a b").to_string(), "(app . ((variable . a) . (variable . b)))" );
+   assert_eq!( parse_expression("123").to_string(), "(literal . 123)" );
    assert_eq!( parse_expression("/([a]+[b])[c]/").to_string(), "(regex . ([a]+[b])[c])" );
    assert_eq!( parse_expression("λx.y").to_string(), "(lambda . ((variable . x) . (variable . y)))" );
+   assert_eq!( parse_expression("λx.y z").to_string(), "(lambda . ((variable . x) . (app . ((variable . y) . (variable . z)))))" );
 }
 
 #[test]
 fn programs() {
-   assert_eq!( parse_program("a := b").to_string(), "(kv ())" );
+   assert_eq!( parse_program("a := b").to_string(), "(kv . ((a  . (variable . b)) . ()))" );
 }
