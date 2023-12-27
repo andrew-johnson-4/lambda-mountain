ab8cefde76edd73fca8e3bb8f001dda13e763672
Andrew Johnson <andrew@subarctic.org>
2023-12-27 14:23:56 -0700

    create a basic test harness to test binary targets

--- src/g.rs
+++ src/g.rs
@@ -7,10 +7,12 @@ the attached permissive MIT license. This license is intended only to protect
 the future development of the project while otherwise allowing people to use
 the code and IP as they would like. Please, just be nice.
 
+G: A Basic Codegen
+
 */
 
-pub mod s; pub use crate::s::*; //S-Expression Library
-pub mod a; pub use crate::a::*; //S-Expression based AST helper functions
-pub mod p; pub use crate::p::*; //Quickly parse input into AST
-pub mod e; pub use crate::e::*; //Evaluate expressions
-pub mod g; pub use crate::g::*; //Generate Code and Compile
+use crate::*;
+
+pub fn compile(cfg: &str, s: &S) -> S {
+   unimplemented!("g::compile")
+}
---
+++ tests/c_bootstrap.rs
@@ -0,0 +1,17 @@
+use std::process::Command;
+use lambda_mountain::*;
+
+fn compile_and_run(s: &S) -> String {
+   compile("a.out", s);
+
+   let output = Command::new("./a.out")
+                            .output()
+                            .expect("failed to execute process")
+                            .stdout;
+   String::from_utf8_lossy(&output).to_string()
+}
+
+#[test]
+fn nil() {
+   assert_eq!( compile_and_run(&s_nil()), "" );
+}
