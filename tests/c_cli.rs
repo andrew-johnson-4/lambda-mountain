62f13ea3a77cd58d7b1921333e24f449e6777432
Andrew Johnson <andrew@subarctic.org>
2024-01-11 05:00:18 -0700

    out-of-order merge is not superior in any ways

--- tests/c_cli.rs
+++ tests/c_cli.rs
@@ -89,8 +89,8 @@ fn control_flow() {
 
 #[test]
 fn dsa() {
-   assert_eq!( compile_and_run("tests/lm/zip.lm"), "(((() 2) 1) 3)" );
-   assert_eq!( compile_and_run("tests/lm/kv_merge.lm"), "(((((() (1 2)) (3 4)) (7 8)) (5 6)) (9 0))" );
+   assert_eq!( compile_and_run("tests/lm/zip.lm"), "(((() 1) 2) 3)" );
+   assert_eq!( compile_and_run("tests/lm/kv_merge.lm"), "(((((() (1 2)) (3 4)) (5 6)) (7 8)) (9 0))" );
    assert_eq!( compile_and_run("tests/lm/kv_lookup.lm"), "456" );
 }
 
