bda4a7241e7d11f753b637e08bc4b4846e0691f8
Andrew Johnson <andrew@subarctic.org>
2024-01-19 22:13:13 -0700

    oops working on main, but it works now

--- tests/c_parser.rs
+++ tests/c_parser.rs
@@ -43,7 +43,13 @@ fn cli_parse_expression() {
    assert_eq!( compile_and_run("--parse-expression","tests/lm/parse_literal1.lm"), "(Literal 123)" );
    assert_eq!( compile_and_run("--parse-expression","tests/lm/parse_literal2.lm"), "(Literal abc)" );
    assert_eq!( compile_and_run("--parse-expression","tests/lm/parse_application.lm"), "(App ((App ((Variable x) (Variable y))) (Variable z)))" );
+   assert_eq!( compile_and_run("--parse-expression","tests/lm/parse_nil.lm"), "Nil" );
    assert_eq!( compile_and_run("--parse-expression","tests/lm/parse_grouped.lm"), "(App ((App ((Variable w) (Variable x))) (App ((Variable y) (Variable z)))))" );
    assert_eq!( compile_and_run("--parse-expression","tests/lm/parse_lambda.lm"), "(Lambda ((Variable x) (Variable y)))" );
    assert_eq!( compile_and_run("--parse-expression","tests/lm/parse_lambda2.lm"), "(Lambda (((Variable x) (Variable y)) (App ((App ((Variable w) (Variable x))) (App ((Variable y) (Variable z)))))))" );
+}
+
+#[test]
+fn cli_parse_program() {
+   assert_eq!( compile_and_run("--parse","tests/lm/parse_program.lm"), "((() (x (Variable y))) (a (Variable b)))" );
 }
