fc149a8d3de5022fc8545e1c0c2c15654751a4fd
Andrew Johnson <andrew@subarctic.org>
2024-01-29 03:21:34 -0700

    really bootstrap

--- build.rs
+++ build.rs
@@ -1,8 +1,20 @@
 fn main() {
    println!("cargo:rerun-if-changed=BOOTSTRAP/cli.s");
    std::process::Command::new("as")
-      .args(&["-o","lm", "BOOTSTRAP/cli.s"])
+      .args(&["-o","lm_raw.o", "BOOTSTRAP/cli.s"])
       .status()
       .expect("as BOOTSTRAP/cli.s failed to execute.");
-      ()
+   std::process::Command::new("ld")
+      .args(&["-o","lm_raw", "lm_raw.o"])
+      .status()
+      .expect("ld BOOTSTRAP/cli.s failed to execute.");
+   std::process::Command::new("rm")
+      .args(&["lm_raw.o"])
+      .status()
+      .expect("rm lm_raw.o failed to execute.");
+   std::process::Command::new("mv")
+      .args(&["lm_raw","$HOME/.cargo/bin/lm"])
+      .status()
+      .expect("rm lm_raw.o failed to execute.");
+   ()
 }
