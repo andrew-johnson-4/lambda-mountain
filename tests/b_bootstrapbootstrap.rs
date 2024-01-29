e28ae2214503790e5f0b855beb12d2c201953f74
Andrew Johnson <andrew@subarctic.org>
2024-01-29 00:30:46 -0700

    what is it working?

--- tests/b_bootstrapbootstrap.rs
+++ tests/b_bootstrapbootstrap.rs
@@ -71,15 +71,14 @@ fn compile_bootstrap() {
    };
 }
 
-fn run_bootstrap(target: &str) -> String {
-   rm("tmp.s");
-   rm("tmp.o");
-   rm("a.out");
-   let exit = Command::new("./bbootstrap")
+fn run_bootstrap(target: &str) -> (String,String) {
+   rm("tmp1.s");
+   rm("tmp2.s");
+   let exit = Command::new("./bootstrap")
                       .stdout(std::process::Stdio::piped())
                       .stderr(std::process::Stdio::piped())
                       .arg("-o")
-                      .arg("tmp.s")
+                      .arg("tmp1.s")
                       .arg(target)
                       .spawn()
                       .expect("failed to execute process")
@@ -87,48 +86,25 @@ fn run_bootstrap(target: &str) -> String {
                       .expect("failed to wait for process");
    if !exit.status.success() {
       let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
-      return format!("./bbootstrap error code while compiling {}: {}", target, stderr);
+      panic!("./bootstrap error code while compiling {}: {}", target, stderr);
    };
-   let exit = Command::new("as")
+   let expected = String::from_utf8_lossy(&exit.stdout).to_string();
+   let exit = Command::new("./bbootstrap")
                       .stdout(std::process::Stdio::piped())
                       .stderr(std::process::Stdio::piped())
                       .arg("-o")
-                      .arg("tmp.o")
-                      .arg("tmp.s")
+                      .arg("tmp2.s")
+                      .arg(target)
                       .spawn()
                       .expect("failed to execute process")
                       .wait_with_output()
                       .expect("failed to wait for process");
    if !exit.status.success() {
       let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
-      return format!("as error code while compiling {}: {}", target, stderr);
+      panic!("./bbootstrap error code while compiling {}: {}", target, stderr);
    };
-   let exit = Command::new("ld")
-                      .stdout(std::process::Stdio::piped())
-                      .stderr(std::process::Stdio::piped())
-                      .arg("-o")
-                      .arg("a.out")
-                      .arg("tmp.o")
-                      .spawn()
-                      .expect("failed to execute process")
-                      .wait_with_output()
-                      .expect("failed to wait for process");
-   if !exit.status.success() {
-      let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
-      return format!("ld error code while compiling {}: {}", target, stderr);
-   };
-   let exit = Command::new("./a.out")
-                      .stdout(std::process::Stdio::piped())
-                      .stderr(std::process::Stdio::piped())
-                      .spawn()
-                      .expect("failed to execute process")
-                      .wait_with_output()
-                      .expect("failed to wait for process");
-   if !exit.status.success() {
-      let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
-      return format!("./a.out error code while running {}: {}", target, stderr);
-   };
-   String::from_utf8_lossy(&exit.stdout).to_string()
+   let actual = String::from_utf8_lossy(&exit.stdout).to_string();
+   (expected, actual)
 }
 
 #[test]
@@ -137,18 +113,13 @@ fn bootsuite() {
    let mut failures = Vec::new();
    for entry in glob("tests/lm/*.lm").unwrap() {
       let path = entry.unwrap().display().to_string();
-      let stdout = path.clone() + ".out";
-      assert!(std::path::Path::new(&stdout).exists(),"Expected stdout not found: {}",stdout);
-      let stdout = std::fs::read_to_string(stdout).unwrap();
-      let stdout = stdout.trim().to_string();
-      let actual = run_bootstrap(&path);
-      let actual = actual.trim().to_string();
-      if stdout != actual {
-         failures.push(( path, stdout, actual ));
+      let (actual,expected) = run_bootstrap(&path);
+      if expected != actual {
+         failures.push(( path, expected, actual ));
       }
    }
-   for (path,stdout,actual) in &failures {
-      eprintln!("TEST {} Expected: {}, Actual: {}", path, &stdout[..std::cmp::min(100,stdout.len())], &actual[..std::cmp::min(100,actual.len())]);
+   for (path,expected,actual) in &failures {
+      eprintln!("TEST {} Expected: {}, Actual: {}", path, &expected[..std::cmp::min(100,expected.len())], &actual[..std::cmp::min(100,actual.len())]);
    }
    assert_eq!( failures.len(), 0 );
 }
