c5af1699a1f59dd7ef91c11e34dab67e611c7680
Andrew Johnson <andrew@subarctic.org>
2024-02-10 01:31:07 -0700

    make tests for typed lm

--- tests/x_onexit.rs
+++
@@ -1,9 +0,0 @@
-use glob::glob;
-
-#[test]
-fn cleanup() {
-   for fp in glob("tmp.*").expect("Could not glob files") {
-   if let Ok(fp) = fp {
-      std::fs::remove_file(fp).expect("Could not delete file");
-   }}
-}
