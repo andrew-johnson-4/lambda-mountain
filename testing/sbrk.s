a09990ec9bc3a4df5d615ccbf8dc7b857e8d202f
Andrew Johnson <andrew@subarctic.org>
2024-02-25 01:27:25 -0700

    there needs to be data in .data or else gnu assembler cant make the section. otherwise brk works in tests.

--- testing/sbrk.s
+++ testing/sbrk.s
@@ -3,26 +3,20 @@
 _start:
 	jmp main
 main:
-	push %rbp
-	mov %rsp, %rbp
-	call uuid_0000000000000004
+	call allocate_100
 	movq $123, 0(%r8)
+	movq $123, 16(%r8)
+	mov %rax, %rdi
 	mov $60, %rax
-	mov $0, %rdi
-	mov $0, %rsi
-	mov $0, %rdx
 	syscall
-uuid_0000000000000004:
+allocate_100:
 	mov $program_break, %r8
-	mov $program_break_counter, %r9
-	mov 0(%r9), %r9
-	add %r9, %r8
-	add $1048576, %r8
-	mov $12, %rax
 	mov %r8, %rdi
+	add $100, %rdi
+	mov $12, %rax
 	syscall
-	sub $1048574, %r8
 	ret
-program_break_counter:
+.data
+current_break:
 	.zero 8
 program_break:
