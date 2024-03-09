fc41398cd3fd89dc57812fa077881b4fcba4106c
Andrew Johnson <andrew@subarctic.org>
2024-03-08 17:24:59 -0700

    add benchmark for fibonacci

---
+++ benchmarks/fibonacci.rs
@@ -0,0 +1,14 @@
+
+fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+fn main() {
+   for argument in std::env::args().skip(1) {
+      fibonacci( argument.parse::<u64>().unwrap_or(0) );
+   }
+}
