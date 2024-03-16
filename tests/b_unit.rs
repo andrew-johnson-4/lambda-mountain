cc131d5aa521107e42e55173378f6b901587c6ea
Andrew Johnson <andrew@subarctic.org>
2024-03-15 21:55:47 -0600

    create test harness for unit tests

---
+++ tests/b_unit.rs
@@ -0,0 +1,101 @@
+use std::process::Command;
+use glob::glob;
+
+fn rm(p: &str) {
+   if std::path::Path::new(p).is_file() {
+      std::fs::remove_file(p).expect(&format!("Could not remove file: {}",p))
+   }
+   assert!( !std::path::Path::new(p).is_file() );
+}
+
+fn compile_compiler() {
+   rm("bootstrap");
+   rm("bootstrap.s");
+   rm("bootstrap.o");
+   rm("production");
+   rm("production.s");
+   rm("production.o");
+   let exit = Command::new("as")
+                      .stdout(std::process::Stdio::piped())
+                      .stderr(std::process::Stdio::piped())
+                      .arg("-o")
+                      .arg("bootstrap.o")
+                      .arg("BOOTSTRAP/cli.s")
+                      .spawn()
+                      .expect("failed to execute process")
+                      .wait_with_output()
+                      .expect("failed to wait for process");
+   if !exit.status.success() {
+      let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
+      panic!("as error code: {}", stderr);
+   };
+   let exit = Command::new("ld")
+                      .stdout(std::process::Stdio::piped())
+                      .stderr(std::process::Stdio::piped())
+                      .arg("-o")
+                      .arg("bootstrap")
+                      .arg("bootstrap.o")
+                      .spawn()
+                      .expect("failed to execute process")
+                      .wait_with_output()
+                      .expect("failed to wait for process");
+   if !exit.status.success() {
+      let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
+      panic!("ld error code: {}", stderr);
+   };
+   let exit = Command::new("./bootstrap")
+                      .stdout(std::process::Stdio::piped())
+                      .stderr(std::process::Stdio::piped())
+                      .arg("-o")
+                      .arg("production.s")
+                      .arg("PRODUCTION/cli.lm")
+                      .spawn()
+                      .expect("failed to execute process")
+                      .wait_with_output()
+                      .expect("failed to wait for process");
+   if !exit.status.success() {
+      let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
+      panic!("lm error code: {}", stderr);
+   };
+   let exit = Command::new("as")
+                      .stdout(std::process::Stdio::piped())
+                      .stderr(std::process::Stdio::piped())
+                      .arg("-o")
+                      .arg("production.o")
+                      .arg("production.s")
+                      .spawn()
+                      .expect("failed to execute process")
+                      .wait_with_output()
+                      .expect("failed to wait for process");
+   if !exit.status.success() {
+      let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
+      panic!("as error code: {}", stderr);
+   };
+   let exit = Command::new("ld")
+                      .stdout(std::process::Stdio::piped())
+                      .stderr(std::process::Stdio::piped())
+                      .arg("-o")
+                      .arg("production")
+                      .arg("production.o")
+                      .spawn()
+                      .expect("failed to execute process")
+                      .wait_with_output()
+                      .expect("failed to wait for process");
+   if !exit.status.success() {
+      let stderr = String::from_utf8_lossy(&exit.stderr).to_string();
+      panic!("ld error code: {}", stderr);
+   };
+}
+
+fn run_unit(unit: &str) {
+   println!( "Run Unit {}", unit )
+}
+
+#[test]
+fn unit_test_suite() {
+   compile_compiler();
+   for entry in glob("tests/unit/*.lm").unwrap() {
+      let entry = entry.unwrap().display().to_string();
+      run_unit(&entry);
+   }
+}
