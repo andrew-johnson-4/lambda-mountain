69ea6c8d4563dfdaba721f2cc1927bfb3f884363
Andrew Johnson <andrew@subarctic.org>
2024-03-26 17:10:37 -0600

    include all symbols

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,10 @@ mod instruction_set_movsx; pub use instruction_set_movsx::*;
 mod instruction_set_push; pub use instruction_set_push::*;
 mod instruction_set_pop; pub use instruction_set_pop::*;
 mod instruction_set_add; pub use instruction_set_add::*;
+mod instruction_set_adc; pub use instruction_set_adc::*;
+mod instruction_set_and; pub use instruction_set_and::*;
+mod instruction_set_or; pub use instruction_set_or::*;
+mod instruction_set_sub; pub use instruction_set_sub::*;
+mod instruction_set_sbb; pub use instruction_set_sbb::*;
+mod instruction_set_xor; pub use instruction_set_xor::*;
+
