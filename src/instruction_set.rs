92a7835bc314d1e8fd74ce06d7bf531f8a6100f4
Andrew Johnson <andrew@subarctic.org>
2024-03-26 15:17:48 -0600

    all moves declared in Rust including generic sized moves

--- src/instruction_set.rs
+++ src/instruction_set.rs
@@ -353,4 +353,81 @@ pub fn mov8gs(_src: impl Constant + GlobalVariable + Fragment + Sized<8>, _dst: 
    Nil {}
 }
 
+pub fn mov1sr(_src: impl Constant + StackVariable + Fragment + Sized<1>, _dst: impl Constant + Register + Fragment + Sized<1>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov2sr(_src: impl Constant + StackVariable + Fragment + Sized<2>, _dst: impl Constant + Register + Fragment + Sized<2>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov4sr(_src: impl Constant + StackVariable + Fragment + Sized<4>, _dst: impl Constant + Register + Fragment + Sized<4>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov8sr(_src: impl Constant + StackVariable + Fragment + Sized<8>, _dst: impl Constant + Register + Fragment + Sized<8>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov1sl(_src: impl Constant + StackVariable + Fragment + Sized<1>, _dst: impl Constant + LocalVariable + Fragment + Sized<1>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov2sl(_src: impl Constant + StackVariable + Fragment + Sized<2>, _dst: impl Constant + LocalVariable + Fragment + Sized<2>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov4sl(_src: impl Constant + StackVariable + Fragment + Sized<4>, _dst: impl Constant + LocalVariable + Fragment + Sized<4>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov8sl(_src: impl Constant + StackVariable + Fragment + Sized<8>, _dst: impl Constant + LocalVariable + Fragment + Sized<8>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov1sg(_src: impl Constant + StackVariable + Fragment + Sized<1>, _dst: impl Constant + GlobalVariable + Fragment + Sized<1>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov2sg(_src: impl Constant + StackVariable + Fragment + Sized<2>, _dst: impl Constant + GlobalVariable + Fragment + Sized<2>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov4sg(_src: impl Constant + StackVariable + Fragment + Sized<4>, _dst: impl Constant + GlobalVariable + Fragment + Sized<4>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov8sg(_src: impl Constant + StackVariable + Fragment + Sized<8>, _dst: impl Constant + GlobalVariable + Fragment + Sized<8>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov1ss(_src: impl Constant + StackVariable + Fragment + Sized<1>, _dst: impl Constant + StackVariable + Fragment + Sized<1>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov2ss(_src: impl Constant + StackVariable + Fragment + Sized<2>, _dst: impl Constant + StackVariable + Fragment + Sized<2>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov4ss(_src: impl Constant + StackVariable + Fragment + Sized<4>, _dst: impl Constant + StackVariable + Fragment + Sized<4>) -> impl Fragment {
+   Nil {}
+}
+pub fn mov8ss(_src: impl Constant + StackVariable + Fragment + Sized<8>, _dst: impl Constant + StackVariable + Fragment + Sized<8>) -> impl Fragment {
+   Nil {}
+}
+
+pub fn movngg<const N: usize>(_src: impl Constant + GlobalVariable + Fragment + Sized<N>, _dst: impl Constant + GlobalVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movngl<const N: usize>(_src: impl Constant + GlobalVariable + Fragment + Sized<N>, _dst: impl Constant + LocalVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movngs<const N: usize>(_src: impl Constant + GlobalVariable + Fragment + Sized<N>, _dst: impl Constant + StackVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movnlg<const N: usize>(_src: impl Constant + LocalVariable + Fragment + Sized<N>, _dst: impl Constant + GlobalVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movnll<const N: usize>(_src: impl Constant + LocalVariable + Fragment + Sized<N>, _dst: impl Constant + LocalVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movnls<const N: usize>(_src: impl Constant + LocalVariable + Fragment + Sized<N>, _dst: impl Constant + StackVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movnsg<const N: usize>(_src: impl Constant + StackVariable + Fragment + Sized<N>, _dst: impl Constant + GlobalVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movnsl<const N: usize>(_src: impl Constant + StackVariable + Fragment + Sized<N>, _dst: impl Constant + LocalVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn movnss<const N: usize>(_src: impl Constant + StackVariable + Fragment + Sized<N>, _dst: impl Constant + StackVariable + Fragment + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+
 
