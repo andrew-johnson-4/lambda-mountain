f3ff03eb09ba4656ab84b341e406e29ad3a47c97
Andrew Johnson <andrew@subarctic.org>
2024-03-26 15:27:49 -0600

    all push pop mnemonics

--- src/instruction_set.rs
+++ src/instruction_set.rs
@@ -191,16 +191,59 @@ pub fn movsx4(_src: impl Constant + Fragment + Register + Sized<4>, _dst: impl C
    Nil {}
 }
 
-pub fn push1(_src: impl Constant + Fragment + Sized<1>) -> impl Fragment {
+pub fn push1c(_src: impl Constant + Fragment + Sized<1>) -> impl Fragment {
    Nil {}
 }
-pub fn push2(_src: impl Constant + Fragment + Sized<2>) -> impl Fragment {
+pub fn push2c(_src: impl Constant + Fragment + Sized<2>) -> impl Fragment {
    Nil {}
 }
-pub fn push4(_src: impl Constant + Fragment + Sized<4>) -> impl Fragment {
+pub fn push4c(_src: impl Constant + Fragment + Sized<4>) -> impl Fragment {
    Nil {}
 }
-pub fn push8(_src: impl Constant + Fragment + Sized<8>) -> impl Fragment {
+pub fn push8c(_src: impl Constant + Fragment + Sized<8>) -> impl Fragment {
+   Nil {}
+}
+pub fn push1r(_src: impl Constant + Register + Sized<1>) -> impl Fragment {
+   Nil {}
+}
+pub fn push2r(_src: impl Constant + Register + Sized<2>) -> impl Fragment {
+   Nil {}
+}
+pub fn push4r(_src: impl Constant + Register + Sized<4>) -> impl Fragment {
+   Nil {}
+}
+pub fn push8r(_src: impl Constant + Register + Sized<8>) -> impl Fragment {
+   Nil {}
+}
+pub fn pushng<const N: usize>(_src: impl Constant + GlobalVariable + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn pushnl<const N: usize>(_src: impl Constant + LocalVariable + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn pushns<const N: usize>(_src: impl Constant + StackVariable + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+
+pub fn pop1r(_src: impl Constant + Register + Sized<1>) -> impl Fragment {
+   Nil {}
+}
+pub fn pop2r(_src: impl Constant + Register + Sized<2>) -> impl Fragment {
+   Nil {}
+}
+pub fn pop4r(_src: impl Constant + Register + Sized<4>) -> impl Fragment {
+   Nil {}
+}
+pub fn pop8r(_src: impl Constant + Register + Sized<8>) -> impl Fragment {
+   Nil {}
+}
+pub fn popng<const N: usize>(_src: impl Constant + GlobalVariable + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn popnl<const N: usize>(_src: impl Constant + LocalVariable + Sized<N>) -> impl Fragment {
+   Nil {}
+}
+pub fn popns<const N: usize>(_src: impl Constant + StackVariable + Sized<N>) -> impl Fragment {
    Nil {}
 }
 
