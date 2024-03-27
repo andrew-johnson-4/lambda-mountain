aabe79f270bc1fff75bdc22a671e9df81e4c7bbf
Andrew Johnson <andrew@subarctic.org>
2024-03-26 20:46:15 -0600

    implement a Rust version of compiler too just for sanity I guess

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 
+
+enum CompileMode { Tokenize, Parse, Compile }
+
+use lambda_mountain::*;
+
+static mut CONFIG_MODE: CompileMode = CompileMode::Compile;
+
 fn main() {
-   //todo, maybe make a script that calls internal lm, as, ld together?
+   let mut inputs = Vec::new();
+   for arg in std::env::args() {
+      if arg=="--tokenize" {unsafe{ CONFIG_MODE = CompileMode::Tokenize; }}
+      else if arg=="--parse" {unsafe{ CONFIG_MODE = CompileMode::Parse; }}
+      else if arg=="--compile" {unsafe{ CONFIG_MODE = CompileMode::Compile; }}
+      else { inputs.push(arg); }
+   }
 }
