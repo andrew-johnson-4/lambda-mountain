2c19411fa263b1436cad04cc33046be0904bb694
Andrew Johnson <andrew@subarctic.org>
2024-03-26 19:03:29 -0600

    Fragment traits for all of the expression fields in lm

--- src/instruction_set.rs
+++ src/instruction_set.rs
@@ -4,8 +4,20 @@ pub enum Reg16 { AX, BX, CX, DX, SI, DI, BP, SP, R8W, R9W, R10W, R11W, R12W, R13
 pub enum Reg32 { EAX, EBX, ECX, EDX, ESI, EDI, EBP, ESP, R8D, R9D, R10D, R11D, R12D, R13D, R14D, R15D }
 pub enum Reg64 { RAX, RBX, RCX, RDX, RSI, RDI, RBP, RSP, R8, R9, R10, R11, R12, R13, R14, R15 }
 
+pub enum FragmentType {
+   And(Vec<FragmentType>),
+   Constructor(String,Vec<FragmentType>),
+   Arrow(Box<FragmentType>,Box<FragmentType>),
+}
+
 pub trait Fragment {}
+pub trait Text { fn text() -> String; }
+pub trait Data { fn data() -> String; }
+pub trait Frame { fn frame() -> String; }
+pub trait Unframe { fn unframe() -> String; }
+pub trait Type { fn fragment_type() -> FragmentType; }
 pub trait Label { fn label_id() -> String; }
+pub trait FrameSize { fn frame_size() -> usize; }
 pub trait Constant {}
 pub trait Literal { fn literal_value() -> String; }
 pub trait Register { fn register_name() -> String; }
