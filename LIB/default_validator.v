182dc3f9ebc81eed9d050f98fd251a3590d32d1c
Andrew Johnson <andrew@subarctic.org>
2024-08-09 16:11:21 -0600

    nomenclature

--- LIB/default_validator.v
+++ LIB/default_validator.v
@@ -2,10 +2,11 @@
 Require Import Coq.Numbers.BinNums.
 
 Structure RegionSlot := { 
-    tt : N;      (* The type of this region *)
-    tt_byte : N; (* The type-byte of this slot *)
+    tt : N;      (* The type of this region represented as an Ordinal *)
+    tt_byte : N; (* The type-byte-index of this slot *)
 }.
 
-Structure StackRegion := {
+Structure Region := {
     known : Z -> RegionSlot;
 }.
+
