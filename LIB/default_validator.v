9d954636a4ef42f83ad95c1bb32beb59d0d63e4b
Andrew Johnson <andrew@subarctic.org>
2024-08-09 13:48:36 -0600

    need an efficient tree

--- LIB/default_validator.v
+++ LIB/default_validator.v
@@ -1,5 +1,14 @@
 
-Theorem my_first_theorem : 1 + 1 = 2.
-Proof.
-  reflexivity.
-Qed.
+Require Import Coq.Numbers.BinNums.
+Require Import MMaps.
+
+Module ZM := MMaps.RBT.Make(Z).
+
+Structure RegionSlot := { 
+    tt : N;      (* The type of this region *)
+    tt_byte : N; (* The type-byte of this slot *)
+}.
+
+Structure StackRegion := {
+    known : ZM;
+}.
