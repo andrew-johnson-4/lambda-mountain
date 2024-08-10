3d2e71af333d4723bc690971bafb8f945c9093e7
Andrew Johnson <andrew@subarctic.org>
2024-08-10 16:44:03 -0600

    lib

--- LIB/default_validator.v
+++ LIB/default_validator.v
@@ -1,13 +1,15 @@
 
 Require Import Coq.Numbers.BinNums.
 
-Structure RegionSlot := { 
+(* Memory Is Denominated in Bytes *)
+Structure RegionByte := { 
     tt : N;      (* The type of this region represented as an Ordinal *)
     tt_byte : N; (* The type-byte-index of this slot *)
 }.
 
+(* Knowledge of a Memory Region is a Partial Function *)
 Structure Region := {
-    known : Z -> RegionSlot;
+    known : Z -> RegionByte;
 }.
 
 (* Simplified Memory State assumes that
@@ -17,5 +19,5 @@ Structure Region := {
 Structure MemoryState := {
     stack_state : Region;
     heap_state : Region;
-    heap_tail : N;
 }.
+
