d009d2f3d957182d9db1bc21c139e60950f55f54
Andrew Johnson <andrew@subarctic.org>
2024-08-11 13:44:08 -0600

    I dont know how to use the subtyping so just change to MMap type directly

--- LIB/default_validator.v
+++ LIB/default_validator.v
@@ -1,25 +1,31 @@
 
 Require Import Coq.Numbers.BinNums.
+From MMaps Require Import MMaps.
+Module ZM := MMaps.RBT.Make(BinInt.Z).
 
 (* Memory Is Denominated in Bytes *)
-Structure RegionByte := { 
-    tt : N;      (* The type of this region represented as an Ordinal *)
-    tt_byte : N; (* The type-byte-index of this byte *)
+Record RegionByte := mkRegionByte { 
+    tt : nat;      (* The type of this region represented as an Ordinal *)
+    tt_byte : nat; (* The type-byte-index of this byte *)
 }.
 
 (* Knowledge of a Memory Region is a Partial Function *)
-Structure Region := {
-    known : Z -> RegionByte;
+Record Region := mkRegion {
+    known : ZM.t BinInt.Z;
 }.
 
 (* Simplified Memory State assumes that
    1. stack space is sufficient (effectively infinite)
    2. sys_brk always succeeds at acquiring more memory (effectively infinite)
  *)
-Structure MemoryState := {
+Record MemoryState := mkMemoryState {
     register_state : Region;
     stack_state : Region;
     frame_state : Region;
     heap_state : Region;
 }.
 
+Definition push_stack (st: MemoryState)(tt: N)(tt_byte: N): MemoryState :=
+   st.
+
+Definition initial_memory_state := mkMemoryState (mkRegion ZM.empty) (mkRegion ZM.empty) (mkRegion ZM.empty) (mkRegion ZM.empty).
