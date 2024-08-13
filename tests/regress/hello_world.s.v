022256fc64a7a914d41a777962bba13fb2d3e2d4
Andrew Johnson <andrew@subarctic.org>
2024-08-12 18:36:43 -0600

    hello

--- tests/regress/hello_world.s.v
+++ tests/regress/hello_world.s.v
@@ -1,29 +1,26 @@
 .global _start
 .text
 _start:
-	push %rbp
-	mov %rsp, %rbp
-	call main
-	mov %rbp, %rsp
-	pop %rbp
-	mov $60, %rax
-	mov $0, %rdi
-	syscall
+        push %rbp
+        mov %rsp, %rbp
+        call main
+        mov %rbp, %rsp
+        pop %rbp
+        mov $60, %rax
+        mov $0, %rdi
+        syscall
+#expect STDOUT.ends_with("Hello World\n")
+#expect EXIT_STATUS=0
 main:
-#Call Fragment mov : Arrow<Cons<Constant+Literal+Sized<8>,Constant+Reg64>,Nil>
-	mov $1, %RAX
-#Call Fragment mov : Arrow<Cons<Constant+Literal+Sized<8>,Constant+Reg64>,Nil>
-	mov $1, %RDI
-#Call Fragment mov : Arrow<Cons<Constant+Literal+Sized<8>,Constant+Reg64>,Nil>
-	mov $uuid_0000000000007838, %RSI
-#Call Fragment mov : Arrow<Cons<Constant+Literal+Sized<8>,Constant+Reg64>,Nil>
-	mov $11, %RDX
-#Call Fragment syscall : Arrow<Nil,Nil>
-	syscall
-	mov %rbp, %rsp
-	sub $8, %rsp
-	ret
+        mov $1, %RAX
+        mov $1, %RDI
+        mov $uuid_0000000000007838, %RSI
+        mov $11, %RDX
+        syscall
+        mov %rbp, %rsp
+        sub $8, %rsp
+        ret
 .data
 uuid_0000000000007838:
-	.ascii "hello_world"
-	.zero 1
+        .ascii "Hello World\n"
+        .zero 1
