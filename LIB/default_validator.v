f93f2cf718aa1f887c8175c793b103af180cc4c1
Andrew Johnson <andrew@subarctic.org>
2024-08-31 15:35:52 -0600

    declare lable

--- LIB/default_validator.v
+++ LIB/default_validator.v
@@ -19,6 +19,7 @@ Record RegionByte := mkRegionByte {
    tt : nat;      (* The type of this region represented as an Ordinal *)
    tt_byte : nat; (* The type-byte-index of this byte *)
 }.
+Definition unknown_region_byte := mkRegionByte 0 0.
 Definition beq_rb (l: RegionByte)(r: RegionByte): bool :=
    (Nat.eqb l.(tt) r.(tt)) && (Nat.eqb l.(tt_byte) r.(tt_byte)).
 Definition beqo_rb (l: option RegionByte)(r: RegionByte): bool := match l with
@@ -29,6 +30,7 @@ Definition beqo_rb (l: option RegionByte)(r: RegionByte): bool := match l with
 Record Region := mkRegion {
    known : ZM.t RegionByte;
 }.
+Definition empty_region := mkRegion ZM.empty.
 
 (* Simplified Memory State assumes that
    1. stack space is sufficient (effectively infinite)
@@ -40,6 +42,7 @@ Record MemoryState := mkMemoryState {
    frame_state : Region;
    heap_state : Region;
 }.
+Definition empty_memory_state := mkMemoryState empty_region empty_region empty_region empty_region.
 
 (* An Instruction is defined by its mnemonic and effect *)
 Record Instruction := mkInstruction {
@@ -57,33 +60,35 @@ Record JmpInstruction := mkJmp {
 Record BasicData := mkData {
    bytes : list nat;
 }.
+Definition empty_data := mkData nil.
 
 (* A Basic Block is a list of instructions with no branches *)
 Record BasicBlock := mkBasicBlock {
    instructions : list Instruction;
    tail : list JmpInstruction;
 }.
+Definition empty_block := mkBasicBlock nil nil.
+
+Inductive ProgramSection : Type :=
+  | TextSection
+  | DataSection.
 
 (* A Control Flow Graph is a set of labelled blocks *)
 Record ControlFlowGraph := mkCFG {
-   section : string;
+   section : ProgramSection;
    current_label : string;
    blocks : list (string * BasicBlock);
    data : list (string * BasicData);
    globals : list string;
 }.
-
-Definition empty_control_flow_graph := mkCFG "text" "_start" nil nil nil.
+Definition empty_control_flow_graph := mkCFG TextSection "_start" nil nil nil.
 
 (* The Type of an unknown RegionByte is Ordinal 0 *)
 Definition region_lookup (r: Region)(i: BinInt.Z): RegionByte := 
    match ZM.find i r.(known) with
    | Some x => x
-   | None => mkRegionByte 0 0
+   | None => unknown_region_byte
    end.
-
-(* Initially nothing is known about the memory state *)
-Definition initial_memory_state := mkMemoryState (mkRegion ZM.empty) (mkRegion ZM.empty) (mkRegion ZM.empty) (mkRegion ZM.empty).
 
 (* This is for internal use, it does not directly correspond to the actual instruction *)
 Definition push_stack (st: MemoryState)(tt: nat)(tt_byte: nat): MemoryState :=
@@ -99,8 +104,8 @@ Definition pop_stack (st: MemoryState): (MemoryState * RegionByte) :=
    let st := mkMemoryState st.(register_state) new_stack st.(frame_state) st.(heap_state) in
    (st , rb).
 
-Check eq_refl : ((pop_stack initial_memory_state) = (_ , {| tt := 0; tt_byte := 0; |})).
-Check eq_refl : (pop_stack (push_stack initial_memory_state 123 456) = (_ , {| tt := 123; tt_byte := 456; |})).
+Check eq_refl : ((pop_stack empty_memory_state) = (_ , {| tt := 0; tt_byte := 0; |})).
+Check eq_refl : (pop_stack (push_stack empty_memory_state 123 456) = (_ , {| tt := 123; tt_byte := 456; |})).
 
 (* Check if one memory state is a subset of another memory state *)
 Definition mem_is_subset (lo: MemoryState)(hi: MemoryState): bool := 
@@ -110,24 +115,21 @@ Definition mem_is_subset (lo: MemoryState)(hi: MemoryState): bool :=
    let rt_heap_state := ZM.fold (fun k e b => b && (beqo_rb (ZM.find k hi.(heap_state).(known)) e)) lo.(heap_state).(known) true in
    rt_register_state && rt_stack_state && rt_frame_state && rt_heap_state.
 
-Check eq_refl : (mem_is_subset initial_memory_state initial_memory_state) = true.
+Check eq_refl : (mem_is_subset empty_memory_state empty_memory_state) = true.
 
 Definition declare_global (cfg: ControlFlowGraph) (glb: string): ControlFlowGraph :=
    mkCFG cfg.(section) cfg.(current_label) cfg.(blocks) cfg.(data) (cons glb cfg.(globals)).
 
 (* This changes the program section to text mode *)
 Definition declare_text (cfg: ControlFlowGraph) := 
-   mkCFG "text" cfg.(current_label) cfg.(blocks) cfg.(data) cfg.(globals).
+   mkCFG TextSection cfg.(current_label) cfg.(blocks) cfg.(data) cfg.(globals).
 
 (* This changes the program section to text mode *)
 Definition declare_data (cfg: ControlFlowGraph) := 
-   mkCFG "data" cfg.(current_label) cfg.(blocks) cfg.(data) cfg.(globals).
+   mkCFG DataSection cfg.(current_label) cfg.(blocks) cfg.(data) cfg.(globals).
 
-(*
 Definition declare_label (cfg: ControlFlowGraph) (glb: string): ControlFlowGraph :=
    match cfg.(section) with
-   | "text" => match 
- mkCFG cfg.(section) glb (cons (glb , (mkBasicBlock nil nil)) cfg.(blocks)) cfg.(data) cfg.(globals)
-   | "data" => mkCFG cfg.(section) glb cfg.(blocks) (cons (glb , (mkBasicBlock nil nil)) cfg.(data)) cfg.(globals)
+   | TextSection => mkCFG cfg.(section) glb (cons (glb , empty_block) cfg.(blocks)) cfg.(data) cfg.(globals)
+   | DataSection => mkCFG cfg.(section) glb cfg.(blocks) (cons (glb , empty_data) cfg.(data)) cfg.(globals)
    end.
-*)
