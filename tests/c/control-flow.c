2791d42c8e023c1e1cd52549ea866eac79eb62eb
andrew-johnson-4 <andrew@subarctic.org>
2025-03-24 16:59:25 -0600

    the parse is wrong

--- tests/c/control-flow.c
+++ tests/c/control-flow.c
@@ -3,14 +3,15 @@ int main() {
    int x, y;
    if(x){ y; };
    if(x){ y; } else { y; };
-   for(;;){ x; };
-   for(int z;;){ x; };
-   for(;x;){ x; };
-   for(;;x){ x; };
-   for(int z; x;){ x; };
-   for(int z;;x){ x; };
-   for(;x;x){ x; };
-   for(int z;x;x){ x; };
+   for(;;){ x + 1; };
+   for(int z;;){ x + 2; };
+   for(int z=2;;){ x + 3; };
+   for(;x;){ x + 4; };
+   for(;;x){ x + 5; };
+   for(int z; x;){ x + 6; };
+   for(int z;;x){ x + 7; };
+   for(;x;x){ x + 8; };
+   for(int z;x;x){ x + 9; };
    while(x){ x; };
    do{x;} while(x);
    switch(1){
