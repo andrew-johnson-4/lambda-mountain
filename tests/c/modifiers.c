b1849a083cc26fad254d4d1d21c3b2f5cbb8858a
andrew-johnson-4 <andrew@subarctic.org>
2025-03-28 17:06:31 -0600

    function definitions are not parsing

--- tests/c/modifiers.c
+++ tests/c/modifiers.c
@@ -1,6 +1,6 @@
 
 typedef int Integer;
-typedef int(*f1)();
+//typedef int(*f1)();
 extern int f2(){}
 static int f3;
 _Thread_local int f4;
@@ -9,9 +9,9 @@ const int f8;
 volatile int f9;
 _Atomic int f10;
 
-int f11(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
-inline int f12(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
-_Noreturn int f13(int *restrict a1, int ** restrict a2, int * restrict * a3);
+//int f11(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
+//inline int f12(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
+//_Noreturn int f13(int *restrict a1, int ** restrict a2, int * restrict * a3);
 
 int main(){
    auto int f6;
