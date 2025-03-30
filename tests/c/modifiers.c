223c4e9443e7a7c395fbffb8bdc171dbd5b9c754
andrew-johnson-4 <andrew@subarctic.org>
2025-03-30 17:22:16 -0600

    typedefs

--- tests/c/modifiers.c
+++ tests/c/modifiers.c
@@ -1,20 +1,20 @@
 
 typedef int Integer;
-typedef int(*f1)();
-typedef int(*f12)(int x);
-extern int f2(){}
-static int f3;
-_Thread_local int f4;
+typedef Integer(*f1)();
+typedef Integer(*f2)(Integer x);
+extern f1 f3(){}
+static f2 f4;
 _Thread_local int f5;
-const int f8;
-volatile int f9;
-_Atomic int f10;
+_Thread_local int f6;
+const int f7;
+volatile int f8;
+_Atomic int f9;
 
-int f11(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
-inline int f12(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
-_Noreturn int f13(int *restrict a1, int ** restrict a2, int * restrict * a3);
+int f10(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
+inline int f11(int *restrict a1, int ** restrict a2, int * restrict * a3) {};
+_Noreturn int f12(int *restrict a1, int ** restrict a2, int * restrict * a3);
 
 int main(){
-   auto int f6;
-   register int f7;
+   auto int f13;
+   register int f14;
 }
