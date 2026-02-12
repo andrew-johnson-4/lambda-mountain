2d94e4c65e5fa9b07065250842dd0142839d1a6d
Andrew Johnson <andrew@subarctic.org>
2026-02-12 16:46:48 -0700

    zero initialize

--- tests/regress.rs
+++ tests/regress.rs
@@ -30,15 +30,15 @@ fn compile_bootstrap() {
    };
 }
 
-fn run_bootstrap(target: &str, leave_tmp: bool, is_v1: bool) -> String {
+fn run_bootstrap(target: &str, leave_tmp: bool, is_v3: bool) -> String {
    if !leave_tmp { rm("tmp.c"); };
    rm("a.out");
    
-   let exit = if is_v1 {
+   let exit = if is_v3 {
       Command::new("./bootstrap.exe")
               .stdout(std::process::Stdio::piped())
               .stderr(std::process::Stdio::piped())
-              .arg("--v1")
+              .arg("--v3")
               .arg("-o")
               .arg("tmp.c")
               .arg(target)
