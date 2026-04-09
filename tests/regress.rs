eac85f765bb2306dced209a08329ee69412940ca
Andrew Johnson <andrew@subarctic.org>
2026-04-09 08:41:40 -0600

    GC is a bit slower now but only 3-4x

--- tests/regress.rs
+++ tests/regress.rs
@@ -34,25 +34,7 @@ fn run_bootstrap(target: &str, leave_tmp: bool, is_v3: bool) -> String {
    if !leave_tmp { rm("tmp.c"); };
    rm("a.out");
 
-   //these exist because GC is slow atm
-   let mut lm_protect = target.contains("lm-");
-   //if target.contains("lm-type") { lm_protect = false; };
-   //if target.contains("lm-ast") { lm_protect = false; };
-   //if target.contains("lm-tctx") { lm_protect = false; };
-    
-   let exit = if is_v3 && lm_protect {
-      Command::new("./bootstrap.exe")
-              .stdout(std::process::Stdio::piped())
-              .stderr(std::process::Stdio::piped())
-              .arg("--v23")
-              .arg("-o")
-              .arg("tmp.c")
-              .arg(target)
-              .spawn()
-              .expect("failed to execute process")
-              .wait_with_output()
-              .expect("failed to wait for process")
-   } else if is_v3 {
+   let exit = if is_v3 {
       Command::new("./bootstrap.exe")
               .stdout(std::process::Stdio::piped())
               .stderr(std::process::Stdio::piped())
